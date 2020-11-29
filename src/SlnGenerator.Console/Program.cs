61cc8d7f657851f21ada67a8059d5da410f31a9a
joooooel <duochjagochpensionarerna@gmail.com>
2020-11-29 21:48:25 +0100

    Fiddling around

--- src/SlnGenerator.Console/Program.cs
+++ src/SlnGenerator.Console/Program.cs
@@ -1,10 +1,89 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.CommandLine;
+using System.CommandLine.Builder;
+using System.CommandLine.Help;
+using System.CommandLine.Invocation;
+using System.CommandLine.IO;
+using System.CommandLine.Parsing;
 using System.IO;
 using System.Linq;
 using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 using Microsoft.Build.Construction;
+
+var command = new RootCommand()
+{
+    Description = "Tjenare",
+};
+
+command.AddCommand(CreateInitCommand());
+command.AddCommand(CreateGoCommand());
+
+// Show commandline help unless a subcommand was used.
+command.Handler = CommandHandler.Create<IHelpBuilder>(help =>
+{
+    help.Write(command);
+    return 1;
+});
+
+var builder = new CommandLineBuilder(command);
+builder.UseHelp();
+builder.UseVersionOption();
+builder.UseDebugDirective();
+builder.UseParseErrorReporting();
+builder.ParseResponseFileAs(ResponseFileHandling.ParseArgsAsSpaceSeparated);
+
+builder.CancelOnProcessTermination();
+// builder.UseExceptionHandler(HandleException);
+
+var parser = builder.Build();
+await parser.InvokeAsync(args);
+
+
+static Command CreateInitCommand()
+{
+    var command = new Command("init", "create a yaml manifest")
+    {
+        CommonArguments.Path_Optional,
+    };
+
+    command.AddArgument(new Argument<string>("hej", "hej"));
+    command.AddOption(new Option(new[] { "-f", "--force" })
+    {
+        Description = "Overrides the tye.yaml file if already present for project.",
+        Required = false
+    });
+
+    command.Handler = CommandHandler.Create<IConsole, FileInfo?, bool>((console, path, force) =>
+    {
+        var watch = System.Diagnostics.Stopwatch.StartNew();
+        
+        var outputFilePath = GenerateYamlFile(path);
+        console.Out.WriteLine($"Created '{outputFilePath}'.");
+
+        watch.Stop();
+
+        TimeSpan elapsedTime = watch.Elapsed;
+
+        console.Out.WriteLine($"Time Elapsed: {elapsedTime.Hours:00}:{elapsedTime.Minutes:00}:{elapsedTime.Seconds:00}:{elapsedTime.Milliseconds / 10:00}");
+    });
+
+    return command;
+}
+
+static Command CreateGoCommand()
+{
+    var command = new Command("go", "go go go go")
+    {
+        new Argument<FileInfo>((r) => TryParsePath(r, required: false), isDefault: true)
+        {
+            Arity = ArgumentArity.ZeroOrOne,
+            Description = "file or directory, can be a yaml, sln, or project file",
+            Name = "path",
+        },
+    };
+}
 
 // var solutionPath = args[0];
 // var solutionPath = "../../../../../../notified/Notified.sln";
@@ -44,11 +123,10 @@ static async Task GenerateSlnFileAsync(string yamlPath)
     // TODO: Conditionally load depenedent projects
 }
 
-static void GenerateYamlFile(string solutionPath)
+static string GenerateYamlFile(FileInfo? solutionFileInfo)
 {
-    Console.WriteLine($"Input: {solutionPath}");
-
-    var solutionFileInfo = new FileInfo(solutionPath);
+    Console.WriteLine($"Input: {solutionFileInfo.FullName}");
+    
     var solutionFile = SolutionFile.Parse(solutionFileInfo.FullName);
     var projects = GetProjectsInSolution(solutionFile);
 
@@ -86,6 +164,7 @@ static void GenerateYamlFile(string solutionPath)
     
     var outputFilePath = Path.Combine(solutionFileInfo.Directory.FullName, "test.yaml");
     File.WriteAllText(outputFilePath, serialized);
+    return outputFilePath;
 }
 
 static IEnumerable<FileInfo> GetProjectsInSolution(SolutionFile solutionFile)
@@ -112,3 +191,72 @@ static bool ContainsOutputTypeExe(FileInfo projectFile)
 
 static string NormalizeServiceName(string name)
     => Regex.Replace(name.ToLowerInvariant(), "[^0-9A-Za-z-]+", "-");
+    
+    internal static class CommonArguments
+    {
+        public static Argument<FileInfo> Path_Optional
+        {
+            get
+            {
+                return new Argument<FileInfo>((r) => TryParsePath(r, required: false), isDefault: true)
+                {
+                    Arity = ArgumentArity.ZeroOrOne,
+                    Description = "file or directory, can be a yaml, sln, or project file",
+                    Name = "path",
+                };
+            }
+        }
+
+        public static Argument<FileInfo> Path_Required
+        {
+            get
+            {
+                return new Argument<FileInfo>((r) => TryParsePath(r, required: true), isDefault: true)
+                {
+                    Arity = ArgumentArity.ZeroOrOne,
+                    Description = "file or directory, can be a yaml, sln, or project file",
+                    Name = "path",
+                };
+            }
+        }
+
+        static FileInfo TryParsePath(ArgumentResult result, bool required)
+        {
+            var token = result.Tokens.Count switch
+            {
+                0 => ".",
+                1 => result.Tokens[0].Value,
+                _ => throw new InvalidOperationException("Unexpected token count."),
+            };
+
+            if (string.IsNullOrEmpty(token))
+            {
+                token = ".";
+            }
+
+            if (File.Exists(token))
+            {
+                return new FileInfo(token);
+            }
+
+            if (Directory.Exists(token))
+            {
+                // if (ConfigFileFinder.TryFindSupportedFile(token, out var filePath, out var errorMessage))
+                // {
+                //     return new FileInfo(filePath);
+                // }
+                // else if (required)
+                // {
+                //     result.ErrorMessage = errorMessage;
+                //     return default!;
+                // }
+                // else
+                // {
+                //     return default!;
+                // }
+            }
+
+            result.ErrorMessage = $"The file '{token}' could not be found.";
+            return default!;
+        }
+    }
